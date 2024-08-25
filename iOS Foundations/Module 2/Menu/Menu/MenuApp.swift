57a89807a65b4f1f875ccce8a2c138f3018fa067
chonhenry <chonhenry@gmail.com>
2024-08-25 19:32:40 -0400

    module 2

---
+++ iOS Foundations/Module 2/Menu/Menu/MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuApp.swift
+//  Menu
+//
+//  Created by Henry Chon on 8/25/24.
+//
+
+import SwiftUI
+
+@main
+struct MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MenuView()
+        }
+    }
+}
---
+++ iOS Foundations/Module 2/Menu/Menu/MenuItem.swift
@@ -0,0 +1,14 @@
+//
+//  MenuItem.swift
+//  Menu
+//
+//  Created by Henry Chon on 8/25/24.
+//
+
+import Foundation
+
+struct MenuItem {
+    var name: String
+    var price: String
+    var imageName: String
+}
